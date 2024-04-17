2c5288d2aa4da5f19b13fa9bc5031f63880ff723
Aslan Mustafayev <116913574+aslanmyn@users.noreply.github.com>
2024-04-17 18:29:28 +0500

    Add files via upload

---
+++ Lab10/hh_back/api/views.py
@@ -0,0 +1,28 @@
+from .models import Company,Vacancy
+from rest_framework import generics
+from .serializer import CompanySerializer, VacancySerializer
+# Create your views here.
+
+
+class CompanyViewSet(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class VacancyViewSet(generics.ListCreateAPIView):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+class TopTenVacanciesListView(generics.ListAPIView):
+
+    serializer_class = VacancySerializer
+    def get_queryset(self):
+        top_vacancies = Vacancy.objects.order_by('-salary')[:10]
+        return top_vacancies
+
+class CompanyVacancyListView(generics.ListAPIView):
+
+    serializer_class = VacancySerializer
+
+    def get_queryset(self):
+        company_id = self.kwargs['company']
+        return Vacancy.objects.filter(company_id=company_id)
